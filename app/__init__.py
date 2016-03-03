bbad1044792b08518d14ea0a0b428fd64c1f22b4
Prerit Garg <prerit2010@gmail.com>
2016-03-03 19:18:01 +0530

    minor cleanups

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask.ext.mysql import MySQL
 
 application = Flask(__name__)
-mysql = MySQL()
 
+mysql = MySQL()
 application.config['MYSQL_DATABASE_USER'] = 'root'
-application.config['MYSQL_DATABASE_PASSWORD'] = 'prerit9910'
+application.config['MYSQL_DATABASE_PASSWORD'] = '12345'
 application.config['MYSQL_DATABASE_DB'] = 'os_database'
 application.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(application)
