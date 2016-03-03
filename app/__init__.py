6984215dce5d55c4b2b443bd6862563625221c9a
Prerit Garg <prerit2010@gmail.com>
2016-03-03 18:45:35 +0530

    minor cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,4 @@
 from flask import Flask
-# from flask.ext.mongoengine import MongoEngine
-# from app_config import Config
 from flask.ext.mysql import MySQL
 
 application = Flask(__name__)
@@ -12,9 +10,6 @@ application.config['MYSQL_DATABASE_DB'] = 'os_database'
 application.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(application)
 con = mysql.connect()
-
-
-
 from app import views
 
 
