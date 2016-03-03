444d42042d8dd4f5a4218cac61e92ff59cb3681b
Prerit Garg <prerit2010@gmail.com>
2016-03-03 18:41:13 +0530

    os info inserted in mysql db

--- app/views.py
+++ app/views.py
@@ -5,22 +5,18 @@ from flask import request
 
 @application.route('/data/', methods = ['POST'])
 def server():
-	system_version = str(request.json['system_version'])
+	
 	system_dist = str(request.json['system_dist'])
 	system = str(request.json['system'])
 	machine = str(request.json['machine'])
 	system_platform = str(request.json['system_platform'])
 	uname = str(request.json['uname'])
 	version = str(request.json['version'])
-	mac_ver = str(request.json['mac_ver'])
-
 	cursor = con.cursor()
-	query = "INSERT INTO `user` (`username`,  `password`) values ('demo_user' , '1234564545789')"
+	query = "INSERT INTO os_info (`system_dist`,  `system` , `machine` , `system_platform` , `uname` , `version`) values (\"" + system_dist + "\",\"" +  system + "\",\""  + machine + "\",\"" +  system_platform + "\",\"" + uname + "\",\"" + version + "\")"
+	print query
 	cursor.execute(query)
 	con.commit()
-	# mysql.connect().commit()
-	print "ok"
-	
 	response = {"status" : "Response Recieved!"}
 	return make_response(jsonify(response))
 
